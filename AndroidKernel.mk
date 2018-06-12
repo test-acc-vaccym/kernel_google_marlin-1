mightymart/kernel_lge_bullhead

make builds reproducible

AndroidKernel.mk
@@ -1,3 +1,7 @@
+export KBUILD_BUILD_TIMESTAMP := $(shell $(DATE))
+export KBUILD_BUILD_USER := user
+export KBUILD_BUILD_HOST := host
+
 #Android makefile to build kernel as a part of Android Build
 PERL		= perl
 

test-acc-vaccym/kernel_google_marlin-1

Change-Id: I027bcf81eba36dffe4f457eebcb3f002f96aa45c

AndroidKernel.mk
@@ -1,3 +1,7 @@
+export KBUILD_BUILD_TIMESTAMP := $(shell $(DATE))
+export KBUILD_BUILD_USER := hardenedos
+export KBUILD_BUILD_HOST := hardenedos
+
 #Android makefile to build kernel as a part of Android Build
 PERL		= perl
 